43661afa73e0fc28fc039e30fe8d1b58071824c8
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-03-03 11:29:47 +0900

    0303

--- src/BOJ/Bronze/Bronze2/_1075.java
+++ src/BOJ/Bronze/Bronze2/_1075.java
@@ -1,8 +1,10 @@
+package BOJ.Bronze.Bronze2;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main{
+public class _1075 {
     public static void main(String[] args) throws IOException {
         BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
 
