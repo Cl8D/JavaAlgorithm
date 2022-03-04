1375a9f572d4c4a1f9e35bcc1b65ebc095c5091c
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-03-04 13:22:41 +0900

    0304

--- src/BOJ/Bronze/Bronze1/_1546.java
+++ src/BOJ/Bronze/Bronze1/_1546.java
@@ -1,11 +1,12 @@
+package BOJ.Bronze.Bronze1;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class _1546 {
     public static void main(String[] args) throws IOException {
         BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(buf.readLine());
