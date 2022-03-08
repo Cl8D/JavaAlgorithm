0373ab904bd91cb07d8bfd03e753d2d00457e6c5
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-03-08 16:26:40 +0900

    0308

--- src/BOJ/Silver/Silver5/_1427.java
+++ src/BOJ/Silver/Silver5/_1427.java
@@ -1,10 +1,12 @@
+package BOJ.Silver.Silver5;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class Main {
+public class _1427 {
     public static void main(String[] args) throws IOException {
         BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
         String n = buf.readLine();
