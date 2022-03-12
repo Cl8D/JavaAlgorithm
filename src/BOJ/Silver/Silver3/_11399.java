8e0f0332bb50fce52ac7c26980673fb7e59ff93e
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-03-12 17:39:56 +0900

    0312

--- src/BOJ/Silver/Silver3/_11399.java
+++ src/BOJ/Silver/Silver3/_11399.java
@@ -1,10 +1,12 @@
+package BOJ.Silver.Silver3;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class _11399 {
     public static void main(String[] args) throws IOException {
         BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(buf.readLine());
