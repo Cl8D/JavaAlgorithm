3c50f013c17a63486020c44b1653e10e3cabc241
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-03-16 22:53:27 +0900

    0316

--- src/BOJ/Silver/Silver2/_10819.java
+++ src/BOJ/Silver/Silver2/_10819.java
@@ -1,10 +1,11 @@
+package BOJ.Silver.Silver2;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-
+public class _10819 {
     static int[] temp, num;
     static boolean[] visited;
     static int result = 0;
