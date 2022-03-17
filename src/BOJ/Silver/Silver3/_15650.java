a437e5b7a4f2c21fe011538f38b68014f31fa0ce
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-03-17 14:03:37 +0900

    0317

--- src/BOJ/Silver/Silver3/_15650.java
+++ src/BOJ/Silver/Silver3/_15650.java
@@ -1,12 +1,12 @@
+package BOJ.Silver.Silver3;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-
+public class _15650 {
     static int n, m;
-    static boolean[] visited;
     static int[] temp;
 
     public static void main(String[] args) throws IOException {
@@ -16,32 +16,24 @@ public class Main {
         n = Integer.parseInt(stk.nextToken());
         m = Integer.parseInt(stk.nextToken());
 
-        visited = new boolean[n];
-        temp = new int[n];
+        temp = new int[m];
 
-        dfs(0,0);
+        dfs(0, 0);
 
     }
 
     public static void dfs(int start, int depth) {
+
         if(depth == m) {
-            for (int i = 0; i < m; i++) {
-                System.out.print(temp[i] + " ");
-            }
+            for(int t : temp)
+                System.out.print(t + " ");
             System.out.println();
             return;
         }
 
         for(int i=start; i<n; i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-
-                temp[depth] = i+1;
-
-                dfs(i+1,depth+1);
-
-                visited[i] = false;
-            }
+            temp[depth] = i+1;
+            dfs(i+1, depth+1);
         }
     }
 }
