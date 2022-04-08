78c9eb0b11a8bcfce6360b0e4f2287be5e15472e
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-04-08 17:32:02 +0900

    0408

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk;
 
-        String answer = "";
-        int[] numbers = {3, 30, 34, 5, 9};
+        int n = Integer.parseInt(buf.readLine());
+        Set<Integer> set = new HashSet<>();
+        stk = new StringTokenizer(buf.readLine());
 
-        Arrays.sort(numbers);
-        
+        for(int i=0; i<n; i++)
+            set.add(Integer.parseInt(stk.nextToken()));
+
+        if(set.size() == n)
+            System.out.println("U");
+        else
+            System.out.println("D");
+
 
     }
 }
