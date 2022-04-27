210bddc2433e5b16b7f02f6c5cffe56ea2623f83
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-04-27 15:45:20 +0900

    :technologist: 0427

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,9 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[][] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk;
         stk = new StringTokenizer(buf.readLine());
-
-        int n = Integer.parseInt(stk.nextToken());
-        int r = Integer.parseInt(stk.nextToken());
-
-        dp = new int[34][34];
-
-        System.out.println(dfs(n,r));
     }
-
-    static int dfs(int n, int r) {
-        if(r==n || r==0) {
-            return 1;
-        }
-        if(dp[n][r] > 0)
-            return dp[n][r];
-        else
-            return dp[n][r] = dfs(n-1, r-1) + dfs(n-1, r);
-    }
-
 }
