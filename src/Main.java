d3cc7213f66dfbe554c179f30c8c5aa8dcbfeb87
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-04-28 14:08:57 +0900

    :triangular_flag_on_post: 0428

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk;
-        stk = new StringTokenizer(buf.readLine());
+
     }
 }
