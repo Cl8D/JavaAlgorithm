398d57a62255897516bacf15ba743d38ebd09232
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-07-12 11:11:23 +0900

    :sparkles: 0712) 11866번 - 요세푸스 문제 0

---
+++ src/BOJ/Silver/Silver5/_11866.java
@@ -0,0 +1,47 @@
+package BOJ.Silver.Silver5;
+
+import java.io.*;
+import java.util.*;
+
+// 요세푸스 문제 0
+public class _11866 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter buf2 = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer stk = new StringTokenizer(buf.readLine());
+
+        int n = Integer.parseInt(stk.nextToken());
+        int k = Integer.parseInt(stk.nextToken());
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=1; i<=n; i++) {
+            queue.add(i);
+        }
+        int temp = 0;
+        StringBuilder sb = new StringBuilder("<");
+        while(!queue.isEmpty()){
+            if(temp==k-1) {
+                sb.append(queue.poll() + ", ");
+                temp = 0;
+            }
+            else {
+                queue.add(queue.poll());
+                temp++;
+            }
+        }
+
+
+        sb.deleteCharAt(sb.length()-1);
+        sb.deleteCharAt(sb.length()-1);
+        sb.append(">");
+
+        buf2.write(sb.toString());
+        buf2.flush();
+
+        buf.close();
+        buf2.close();
+
+    }
+
+}
