9e6729e9d10f98b10e7e18ed9b7d9a51b7d8431e
Jiwon Lee <46810837+Cl8D@users.noreply.github.com>
2022-12-04 21:49:05 +0900

    ✨️ 1204) 15688번 - 수 정렬하기 5

---
+++ src/BOJ/Silver/Silver5/_15688.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+// 수 정렬하기 5
+public class _15688 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int n = Integer.parseInt(br.readLine());
+		int nums[] = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			nums[i] = Integer.parseInt(br.readLine());
+		}
+
+		Arrays.sort(nums);
+
+		for (int i = 0; i < n; i++) {
+			sb.append(nums[i]).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
