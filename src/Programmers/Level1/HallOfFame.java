91c4f0da8955c1b455e54c6c87d4426794322e3f
jiwon <ljwon77@daum.net>
2022-12-20 18:01:50 +0900

    :sparkles: 1220) 프로그래머스 Lv1 - 명예의 전당 (1)

---
+++ src/Programmers/Level1/HallOfFame.java
@@ -0,0 +1,41 @@
+package Programmers.Level1;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+// 명예의 전당 (1)
+public class HallOfFame {
+    public static void main(String[] args) {
+        int k = 3;
+        int[] score = {10, 100, 20, 150, 1, 100, 200};
+        System.out.println(Arrays.toString(new Solution().solution(k, score)));
+    }
+
+    static class Solution {
+        public int[] solution(int k, int[] score) {
+            int[] answer = new int[score.length];
+
+            PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+            pq.offer(score[0]);
+
+            for (int i = 1; i < score.length; i++) {
+                if (i <= k - 1) {
+                    answer[i - 1] = pq.peek();
+                    pq.offer(score[i]);
+                    continue;
+                }
+
+                if (pq.peek() > score[i]) {
+                    answer[i - 1] = pq.peek();
+                    continue;
+                }
+
+                pq.offer(score[i]);
+                answer[i - 1] = pq.poll();
+            }
+
+            answer[score.length - 1] = pq.poll();
+            return answer;
+        }
+    }
+}
