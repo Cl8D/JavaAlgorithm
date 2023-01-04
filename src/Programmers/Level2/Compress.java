35795de3fb7c2085d38c6f9ec31de10803cae275
jiwon <ljwon77@daum.net>
2023-01-04 22:17:10 +0900

    :sparkles: 0104) 프로그래머스 Lv2 - 압축 (주석 추가)

--- src/Programmers/Level2/Compress.java
+++ src/Programmers/Level2/Compress.java
@@ -4,9 +4,14 @@ import java.util.*;
 
 // 압축
 class Compress {
+
+    public static void main(String[] args) {
+        int[] answer = solution("KAKAO");
+        System.out.println(Arrays.toString(answer));
+    }
     private static final Map<String, Integer> indexes = new HashMap<>();
 
-    public int[] solution(String msg) {
+    public static int[] solution(String msg) {
         int[] answer = {};
         initIndex();
 
@@ -19,8 +24,9 @@ class Compress {
         for (int i = 0; i < msgs.length; i++) {
             String value = msgs[i];
 
-            // 만약 글자가 사전에 있다면
+            // 글자가 사전에 없을 때까지 반복한다.
             while (indexes.containsKey(value)) {
+                // 다음 글자를 뽑기 위해 인덱스를 증가시킨다.
                 i++;
                 // 마지막에 도달했을 경우를 체크한다.
                 if (i == msg.length()) {
@@ -36,10 +42,13 @@ class Compress {
                 break;
             }
 
-            // 압축시킨 문자열을 뽑아낸 다음에 사전에 등록한다.
+            // 이전의 문자열에 대해서 출력해준다 (정답 리스트에 담기)
             String compress = value.substring(0, value.length() - 1);
             tempAns.add(indexes.get(compress));
+            // 다음 글자를 포함하여 새롭게 만들어진 문자에 대해서 사전에 넣는다.
             indexes.put(value, ++lastIndex);
+
+            // 다음 글자를 뽑기 위해 증가시킨 인덱스를 다시 감소시킨다.
             i--;
         }
 
@@ -52,7 +61,7 @@ class Compress {
         return answer;
     }
 
-    private void initIndex() {
+    private static void initIndex() {
         // A -> 65 (아스키)
         for (int i=0; i<26; i++) {
             char value = (char)(65 + i);
