355360fa5cd3674e24b1cb26b50fe326f7a558aa
Jiwon Lee <46810837+Cl8D@users.noreply.github.com>
2023-03-15 23:31:25 +0900

    ✨️feat: 1713번: 후보 추천하기(아직 구현 중)

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,55 @@
+
 import java.io.*;
 import java.util.*;
+import java.util.PriorityQueue;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter buf2 = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer stk;
 
-        buf.close();
-        buf2.close();
+  public static void main(String[] args) throws IOException {
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk;
+        int n = Integer.parseInt(buf.readLine());
+        int k = Integer.parseInt(buf.readLine());
+        PriorityQueue<Type> pq = new PriorityQueue<>((o1, o2) -> {
+          if (o1.count == o2.count) {
+            return o2.time - o1.time;
+          }
+          return o1.count - o2.count;
+        });
+        int time = 0;
+        for(int i=0; i<k; i++) {
+           int student = Integer.parseInt(buf.readLine());
+           Type type = new Type(student, 0, 0);
+           if (!pq.contains(type)) {
+             pq.add(new Type(student, 1, time++));
+             continue;
+           }
+          
+        }
+        }
+        static class Type {
+          int number;
+          int count;
+          int time;
+          
+          public Type(int number, int count, int time) {
+            this.number = number;
+            this.count = count;
+            this.time = time;
+          }
+          
+          @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Type type = (Type) o;
+        return Objects.equals(number, type.number);
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
+        }
 }
