024c3bab567dcd4fd115e2501241f3057213c8a8
jiwon <ljwon77@daum.net>
2023-04-07 17:49:31 +0900

    :sparkles: 0407) LeetCode(medium): Longest Consecutive Sequence

---
+++ src/leetcode/medium/LongestConsecutiveSequence.java
@@ -0,0 +1,30 @@
+package leetcode.medium;
+
+import java.util.*;
+
+// Longest Consecutive Sequence
+class LongestConsecutiveSequence {
+    public int longestConsecutive(int[] nums) {
+        if (nums.length < 2) {
+            return nums.length;
+        }
+
+        int answer = 1;
+        Arrays.sort(nums);
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i-1] == nums[i]) {
+                continue;
+            }
+            if (nums[i-1] + 1 == nums[i]) {
+                count++;
+            } else {
+                answer = Math.max(answer, count);
+                count = 1;
+            }
+        }
+
+        answer = Math.max(answer, count);
+        return answer;
+    }
+}
