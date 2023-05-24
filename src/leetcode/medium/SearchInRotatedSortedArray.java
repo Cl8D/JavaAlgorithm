d48b746c53f4cd633b7b3e1b1d1ba1fb30f2a224
jiwon <ljwon77@daum.net>
2023-05-24 13:00:00 +0900

    :sparkles: 0524) LeetCode(medium): Search In Rotated Sorted Array

--- src/leetcode/medium/SearchInRotatedSortedArray.java
+++ src/leetcode/medium/SearchInRotatedSortedArray.java
@@ -39,3 +39,50 @@ class SearchInRotatedSortedArray {
         return map.get(answer);
     }
 }
+
+/**
+ * 그 외에 이런 식으로도 푸는 방법이 있다.
+ */
+
+/**
+public int search(int[] nums, int target) {
+    // Creating ArrayList
+    ArrayList<Integer> clist = new ArrayList<>();
+
+    // adding elements of array
+    // to ArrayList
+    for (int i : nums)
+        clist.add(i);
+
+    // returning index of the element
+    return clist.indexOf(target);
+}
+*/
+
+/**
+public int search(int[] nums, int target) {
+    int size = nums.length;
+    int i =0;
+    int j = size-1;
+    while(i<=j){
+        if(nums[i]==target){
+            return i;
+        }
+        else if(nums[j]==target){
+            return j;
+        }
+        else if(nums[i]+nums[j]>target){
+            j--;
+        }
+        else if(nums[i]+nums[j]<target){
+            i++;
+        }
+        else if(nums[i]+nums[j]==target){
+            i++;
+            j--;
+        }
+    }
+
+    return -1;
+}
+*/
